34c0b56ae0a2dec3a0714bce67efde2d718e2d05
Agnieszka Stec <agnieszkaa.stec@gmail.com>
2019-09-23 16:21:57 +0200

    mister cocktail edit

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -2,47 +2,59 @@ class CocktailsController < ApplicationController
   def index
     @cocktails = Cocktail.all
 
-#     cocktail[:name] == params[:name]
+    #  cocktail[:name] == params[:name]
+  end
 
-end
-
- def show
+  def show
     @cocktail = Cocktail.find(params[:id])
     @dose = Dose.new
     # redirect_to
     # raise
   end
 
-def new
-  @cocktail = Cocktail.new
-end
-
-def create
-  @cocktail = Cocktail.new(strong_params)
-  if @cocktail.save
-    redirect_to cocktail_path(@cocktail)
-  else
-    render :new
+  def new
+    @cocktail = Cocktail.new
   end
-end
+
+  def create
+    @cocktail = Cocktail.new(strong_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
 # def search
 #   raise
 #   @cocktails = cocktails.select do |id, cocktail|
 #     cocktail[:name] == params[:name]
 #   end
-# end
+#   end
 
-def update
-    @restaurant = Restaurant.find(params[:id])
+  def search
+    @cocktails = Cocktail.where(params[:name])
   end
-def destroy
-   @coctail = Cocktail.find(params[:id])
-end
 
-  private
+  def edit
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def update
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.update(strong_params)
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+    redirect_to cocktails_path
+  end
+
+private
 
   def strong_params
     params.require(:cocktail).permit(:name, :photo)
+      # dose_attributes: [:description, :ingredient, :cocktail_id, :ingredient_id])
+  end
 end
-end
-
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,13 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :cocktails do
+    collection do
+      get 'search', to: 'cocktail#search'
+    end
+    # collection :cocktails do
+    #   get 'search'
+
   resources :doses, only: [:create]
 end
-  resources :doses, only: [:destroy]
+resources :doses, only: [:destroy]
 end
-
